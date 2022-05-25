59499f124d08c803f95aaf100bd5b6355b7245d9
Chaelea Rennie <101679339+ChaeleaRennie@users.noreply.github.com>
2022-05-24 20:43:30 -0500

    Codewars Challenge answer for 5-24
    Prompt:
    Complete the solution so that it reverses the string passed into it.
    
    'world'  =>  'dlrow'
    'word'   =>  'drow'

---
+++ 5-24.js
@@ -0,0 +1,13 @@
+/*Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+function solution(str){
+    let newString = "";
+    for (i = str.length -1; i >= 0; i--){
+      newString += str[i]
+    }
+    return newString;
+  }
